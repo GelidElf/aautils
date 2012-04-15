8cd9d538cedbb732435cfbc4baa88bc007499cda
GelidElf <GelidElf@3296e6d6-c073-cc97-8a33-8c2e4d1144f7>
2012-04-15 20:50:19 +0000

    Cargando agente y experimienter y modificando el agente clustering para que cargue del archivo correctamente

--- aautils/src/AgenteClustering.java
+++ aautils/src/AgenteClustering.java
@@ -1,64 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+public class AgenteClustering extends Agente {
 
-public class AgenteClustering {
+	/**
+	 * Constructor
+	 * @See {@link Agente}
+	 * @param codSimulacion
+	 * @param nombreMercado
+	 * @param nombreEquipo
+	 */
+	public AgenteClustering(String codSimulacion, String nombreMercado, String nombreEquipo) {
+		super(codSimulacion, nombreMercado, nombreEquipo);
+	}
 
-	private Map<String, List<Float>> centroids = new HashMap<String, List<Float>>();
-	private int numberOfClusters = 0;
-	
-	private enum readingState {initial,centroidConfigurationReached,centroidConfigurationDataReached,end};
-	private readingState currentState = readingState.initial;
-	
-	private final String CENTROID_CONFIGURATION_START = "Cluster centroids:";
-	private boolean ignoreLines = false;
-	private String mustContainToStopIgnoring = "";
-	
-	
-	public AgenteClustering (){
-	}
-	
-	private void processConfigurationFile(){
-		
-		String line = "";
-		if (!ignoreLines){
-			processLine(line);
-		}else{
-			if (line.contains(mustContainToStopIgnoring)){
-				ignoreLines = false;
-			}
-		}
-		
-	}
-	
-	private void processLine(String line){
-		switch (currentState) {
-		case initial:
-			if (line.trim().startsWith(CENTROID_CONFIGURATION_START))
-				currentState = readingState.centroidConfigurationDataReached;
-			break;
-		case centroidConfigurationDataReached:
-			String[] words = line.trim().split(" ");
-			String key = words[0];
-			List<Float> centroidValues = new ArrayList<Float>();
-			if (words.length > 3){
-				for (int i = 2; i < words.length; i++) {
+	private static final String SPLIT_CHARACTER = ",";
+	private static final String NOMBRE_ARCHIVO_CENTROIDES = "kmediasRanking.txt";
+
+	private static Map<String, List<Float>> centroids = new HashMap<String, List<Float>>();
+	private static int numberOfClusters = 0;
+
+	static {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(NOMBRE_ARCHIVO_CENTROIDES));
+			String str = null;
+			while ((str = in.readLine()) != null) {
+				String[] words = str.trim().split(SPLIT_CHARACTER);
+				numberOfClusters = words.length - 1; // TODO: Check if all clusters have a centroid for all decisions!!!
+				String key = words[0];
+				List<Float> centroidValues = new ArrayList<Float>();
+				for (int i = 1; i < words.length; i++) {
 					centroidValues.add(new Float(words[i]));
 				}
+				centroids.put(key, centroidValues);
 			}
-			centroids.put(key, centroidValues);
-			break;
-		case centroidConfigurationReached:
-			ignoreLines = true;
-			mustContainToStopIgnoring = "======";
-			break;
-		case end:
-			// don't do 
-			break;
-		default:
-			break;
+		} catch (IOException e) {
+
 		}
+		System.out.println("Cargados centroides");
 	}
+
+	public static void main(String[] args) {
+		new AgenteClustering("", "", "");
+	}
+
 }
